d0eaa0d6b9a2a1510069bfb5ae891e5aeb5ac137
Jonas <jonas-kh2@web.de>
2018-02-02 11:23:00 +0100

    SQL Update True/False

--- Beihilfe/src/db/models/SQL_querys_update.java
+++ Beihilfe/src/db/models/SQL_querys_update.java
@@ -13,8 +13,172 @@ public class SQL_querys_update {
 		
 		try
 	    {
-			String query = "UPDATE Rechnungen SET AV=true WHERE RechnungenID=" + rechnungID;
+			String query = "UPDATE Rechnungen SET AV=? WHERE RechnungenID=?";
 			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, true);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void AV_False(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET AV=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, false);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void AB_True(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET AB=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, true);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void AB_False(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET AB=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, false);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void VV_True(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET VV=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, true);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void VV_False(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET VV=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, false);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void BV_True(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET BV=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, true);
+			preparedStmt.setInt(2, rechnungID);
+			
+			conn.commit();
+			preparedStmt.close();
+			conn.close();
+	    }
+	    catch (SQLException sqle)
+	    {
+	      dc.printSQLException(sqle);
+	    }
+	}
+	
+	public static void BV_False(int rechnungID) {
+		
+		Derby_Conn dc = new Derby_Conn();
+		Connection conn = dc.start(Derby_Conn.getStandardDB());
+		
+		try
+	    {
+			String query = "UPDATE Rechnungen SET BV=? WHERE RechnungenID=?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			
+			preparedStmt.setBoolean(1, false);
+			preparedStmt.setInt(2, rechnungID);
 			
 			conn.commit();
 			preparedStmt.close();
